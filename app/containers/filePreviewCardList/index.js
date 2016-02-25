ab06ff2e3cc8ea5e704c968418bcffd7f3d0bd46
Kielan <ki.lemons@gmail.com>
2016-02-25 13:42:18 +0700

    just need to remvoe file preview child to parent communication avoiding redux saga and sticking to event at first

--- app/containers/filePreviewCardList/index.js
+++ app/containers/filePreviewCardList/index.js
@@ -13,14 +13,22 @@ class FilePreviewCardList extends React.Component {
     }
     
     render() {
-		var PreviewCards = this.props.files.map(function(file) {
+	var PreviewCards = this.props.files.map(function(file) {
 	    return (
 		    <FilePreviewCard data={file}/>
 	    )
 	})
-
+	
+    	var checkPreview = function(fileProp) {
+	    console.log(fileProp)
+	    var displayOrNo = (fileProp != 0) ? "block" : "none";
+	    
+	    return {
+		display: displayOrNo
+	    }
+	}
 	    return (
-		    <div className={styles.thumbnailContainer}>
+		    <div style={checkPreview(this.props.files)} className={styles.thumbnailContainer}>
 		    {PreviewCards}
 		    </div>
 	    )
