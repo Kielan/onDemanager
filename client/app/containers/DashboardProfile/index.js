b5796a267b0ec0c7b47063763ae20bde6d6c890b
Kielan <ki.lemons@gmail.com>
2016-03-10 18:23:09 +0700

    some lost styling

--- client/app/containers/DashboardProfile/index.js
+++ client/app/containers/DashboardProfile/index.js
@@ -5,43 +5,55 @@ import styles from './styles.css';
 class DashboardProfile extends React.Component {
       render() {
 	  return (
+		  <div >
 		  <div className={ styles.dashboardLeft}>
 		  <a className={ styles.cover }></a>
-		  
-		  <div>
+		  <div >
+
 		  <a className={styles.DashboardProfileAvatar}>
 		  <img className={styles.profileAvatarImage} src="https://pbs.twimg.com/profile_images/694099768834797568/IvPKkR0E_bigger.jpg"></img>
 		  </a>
-		  <div className={styles.DashboardProfileUsername}>		  
-		  <span className={styles.UsernameSpan}>
-		  <a className={styles.UsernameA}>@<span>KielanLemons</span></a>
+
+	      	  <div className={styles.DashboardProfileAvatarName}>
+		  <a>Kielan Lemons</a>
+		  <span className={styles.DashboardProfileAvatarID}>
+		  <a>@<span>KielanLemons</span></a>
 		  </span>
 		  </div>
-		  </div>
 		  
-		  <div>
+
+		  <div className={styles.DashboardProfileContent}>
+		  
 
 
 		  <div className={styles.stats}>
-		  <a>
-		  <span>Tweets</span>
-		  <span>2,596</span>
+		  <a className={styles.statsItem}>
+		  <span className={styles.statsItemName}>Tweets</span>
+		  <span className={styles.statsItemCount}>2,596</span>
 		  </a>
-		  <a>
-		  <span>Following</span>
-		  <span>2,596</span>
+		  <a className={styles.statsItem}>
+		  <span className={styles.statsItemName}>Following</span>
+		  <span className={styles.statsItemCount}>2,596</span>
 		  </a>
-		  <a>
-		  <span>Followers</span>
-		  <span>200</span>
+		  <a className={styles.statsItem}>
+		  <span className={styles.statsItemName}>Followers</span>
+		  <span className={styles.statsItemCount}>200</span>
 		  </a>
 		  </div>
-		  
-		  <div>
-		  <span className="locationText">Bangkok</span>
-		  <span className="">Joined June 2013</span>
+
+		  <div className={styles.DashboardProfileBio}>
+		  <span>Imagination is its own form of courage</span>
+		  </div>
+	      
+		  <div className={styles.DashboardProfileInfo}>
+		  <i className="fa fa-globe"></i><span className="locationText">Bangkok</span>
+		  </div>
+		  <div className={styles.DashboardProfileInfo}>
+		  <i className="fa fa-calendar"></i><span className="">Joined June 2013</span>
 		  </div>
 		  
+		  </div>
+		  </div>
 		  </div>
 		  </div>
 	  )
--- client/app/containers/ProfileCanopy/index.js
+++ client/app/containers/ProfileCanopy/index.js
@@ -12,15 +12,16 @@ class ProfileCanopy extends React.Component {
 	return (
 		<section>
 		<div className={styles.Cover+ " row"}>
-		
+		<img alt="" src="https://pbs.twimg.com/profile_banners/1506367692/1454496995/1500x500" />
 	    </div>
 		<nav className="row">
-		<div className="col-md-5 col-md-offset-4">
-		<ul>
-		<li><a><span>Bits</span><span>2,623</span></a></li>
-		<li><a><span>Following</span><span>575</span></a></li>
-		<li><a><span>Followers</span><span>263</span></a></li>
-		<li><a><span>Likes</span><span>5,575</span></a></li>
+		<div className="col-md-5 col-md-offset-3">
+		<ul className={styles.profileNav}>
+		<li><a><span className={styles.label}>Bits</span><span className={styles.value}>2,623</span></a></li>
+		<li><a><span className={styles.label}>Following</span><span className={styles.value}>575</span></a></li>
+		<li><a><span className={styles.label}>Followers</span><span className={styles.value}>263</span></a></li>
+		<li><a><span className={styles.label}>Likes</span><span className={styles.value}>5,575</span></a></li>
+		<li><div className={styles.editProfile}><button><span>Edit profile</span></button></div></li>
 	    </ul>
 	    </div>
 		</nav>
